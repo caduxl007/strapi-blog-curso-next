9720fceeae88d2c14d1c86ca1762e026c527de6c
caduxl007 <eduardo2020dev@gmail.com>
2021-03-10 21:34:39 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '892b610658bc0bdf0d17850f0716895d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7b6c9647-9ff2-48ec-af1d-8e91c172ba82'
+};
